c6b62e85720a2bce5d417234709d7fe42ca859a4
tarynblakemiller <yourfriendtaryn@gmail.com>
2020-09-24 15:29:18 -0400

    updated seed-file-database-models

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -2,8 +2,6 @@ const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-///THIS IS MARTA's Handly dandy test for out very first branch > change > push > merge as a team family.
-
 const User = db.define('user', {
   name: {
     type: Sequelize.STRING,
